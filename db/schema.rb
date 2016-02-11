835e25fb3864e2e159342859d3d2bdb83c5da838
James Belden <jbelden@jbelden-mbp.stp01.office.gdi>
2016-02-11 13:21:50 -0600

    Adding new send_email method to offenses_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151226132132) do
 
   create_table "active_admin_comments", force: :cascade do |t|
