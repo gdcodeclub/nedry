4fe9cd714db213780a8681ab4cea2cb5a73c2217
Erik Braegelmann <erik.braegelmann@govdelivery.com>
2016-03-08 08:21:26 -0600

    added format validation to the model

--- app/models/offense.rb
+++ app/models/offense.rb
@@ -1,2 +1,5 @@
 class Offense < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.']+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
