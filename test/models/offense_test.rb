2bb5d9784a83647cfaee5013b9d512937538bf2d
Erik Braegelmann <erik.braegelmann@govdelivery.com>
2016-03-08 09:10:09 -0600

    added some tests (after the fact, of course)

--- test/models/offense_test.rb
+++ test/models/offense_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class OffenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @offense=Offense.new
+  end
+
+  test 'should allow blank emails' do
+    @offense.email = ''
+    assert_equal @offense.valid?, true
+  end
+
+  test 'should fail on invalid email addresses' do
+    addresses = %w[asdf1231 blargh@com.com@.com test_at_example_dot_com test@example,com test@test+test.com]
+    addresses.each do |invalid_address|
+      @offense.email = invalid_address
+      assert_equal @offense.valid?, false, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test 'should allow valid addresses' do
+    addresses = %w[patrick.o'brien@example.ie test@example.com george@a.cz what@what.com]
+    addresses.each do |valid_address|
+      @offense.email = valid_address
+      assert_equal @offense.valid?, true, "#{valid_address.inspect} should be valid"
+    end
+  end
+
 end
