1dda16ab73ff4f54b9c0093bd9f59d1e093a51f3
Erik Braegelmann <erik.braegelmann@govdelivery.com>
2016-03-08 08:30:02 -0600

    allow blank email addresses

--- app/models/offense.rb
+++ app/models/offense.rb
@@ -1,5 +1,5 @@
 class Offense < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.']+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX, allow_blank: true }
 end
